67bfcfe68740d188546ab2b720a0fd4949aab81f
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2014-12-25 06:18:37 -0600

    Finished options

---
+++ options/js/main.js
@@ -0,0 +1,95 @@
+$(document).foundation();
+
+function saveOptions(updatedDiffObj) {
+  chrome.storage.sync.set(updatedDiffObj, function() {
+    console.log('Settings saved');
+  });
+}
+
+chrome.storage.sync.get(null, function(c){
+  console.log(c);
+  if (c.mode !== "simple" && c.mode !== "advanced") { // No prior settings found
+    document.mainForm.modeSelectS.checked = true; // set mode on page
+    document.mainForm.modeSelectA.checked = false;
+    saveOptions({ mode:"simple", calendar_ids: [], notificationClickEdit: false, locale: "en-us" });
+  } 
+  
+  else { // Restore options from storage
+    // mode
+    if (c.mode === "simple") {
+      document.mainForm.modeSelectS.checked = true;
+      document.mainForm.modeSelectA.checked = false;
+      $('#slide-advanced').css("visibility", "hidden");
+    } else if (c.mode === "advanced") {
+      document.mainForm.modeSelectA.checked = true;
+      document.mainForm.modeSelectS.checked = false;
+    }
+  }
+});
+
+// Page creation time - clear one
+if ($('input[name=mode]:checked').val() === "advanced") {
+  $('#slide-advanced').addClass("pullDown");
+  $('#simpleLabel').addClass('fade');
+  $('#advancedLabel').removeClass('fade');
+  saveOptions({ mode: "advanced" }); // options.mode
+} else if (($('input[name=mode]:checked').val() === "simple")) {
+  $('#slide-advanced').removeClass("pullDown");
+  $('#advancedLabel').addClass('fade'); 
+  $('#simpleLabel').removeClass('fade'); 
+  saveOptions({ mode: "simple" }); // options.mode
+}
+
+// Transition 
+$('input[name=mode]').click(function() {
+  if ($('input[name=mode]:checked').val() === "advanced") {
+    $('#slide-advanced').addClass("pullDown");
+    $('#simpleLabel').addClass('fade');
+    $('#advancedLabel').removeClass('fade');
+    $('#slide-advanced').css('visibility','visible');
+  } else if ($('input[name=mode]:checked').val() === "simple") {            
+    $('#slide-advanced').removeClass("pullDown");
+    $('#advancedLabel').addClass('fade');
+    $('#slide-advanced').css('visibility','hidden');
+    $('#simpleLabel').removeClass('fade');
+  }
+  saveOptions({ mode: $('input[name=mode]:checked').val() }); // options.mode
+});
+
+// Calendar names and ids
+var app = angular.module('calIDs', ['xeditable']);
+app.run(function(editableOptions) {
+  editableOptions.theme = 'bs2';
+});
+
+app.controller('main', function($scope, $timeout){ 
+  $scope.cals = [];
+  
+  chrome.storage.sync.get(null, function(c) {
+    if (c.calendar_ids.length !== 0) {
+      $scope.cals = c.calendar_ids;
+    }
+    $scope.$apply();
+  });
+  
+  // + Add button
+  $scope.newCal = function(){
+    $scope.cals.push({name:'', id: '', isNew: true});
+    console.log($scope.cals);
+    saveOptions({ calendar_ids: $scope.cals });
+  };
+  
+  // Update button
+  $scope.updateCal = function(){
+    // Real updating is taken care of by library, only do saving
+    console.log($scope.cals);
+    saveOptions({ calendar_ids: $scope.cals });
+  }
+  
+  // - Delete button
+  $scope.removeCal = function(i){
+    $scope.cals.splice(i,1);
+    console.log($scope.cals);
+    saveOptions({ calendar_ids: $scope.cals });
+  };
+}); 
--- src/bg/background.js
+++ src/bg/background.js
@@ -9,21 +9,23 @@
   var current_req_res_context = {
     edit_url: "", // On notification click
     current_text: "", // for retrying
+    calendar_id: "",
+    caller_cal_nickname: ""
   };
-
+  
   var paths = {
     root: "../../../",
     remote_cal_date_notification_url: "http://raw.githubusercontent.com/nishanths/QuickCal/master/icons/classic/calendars-notification/",
     dark_icon: "icons/dark/512.png"
   };
-
+  
   var options = {
     locale: "en-us",
     notificationClickEdit: false,
     mode: "simple",
-    calendar_ids: {
-      "primary": "nishanth.gerrard@gmail.com"
-    }
+    calendar_ids: [
+      { name : "primary", id : "primary" }
+    ]
   };
   
   /******************************* EVENT LISTENERS ******************************/ 
@@ -45,28 +47,70 @@
     }
   }); 
   
-  // Open Cal with edit on clicking the Edit Event button  
+  // Open Cal with edit on clicking the Edit Event, Try again, and help buttons 
   chrome.notifications.onButtonClicked.addListener(function(notificationId, buttonIndex){
     if (notificationId === "atc-addtocal-n-addactionresult") {
       if (buttonIndex === 0) { chrome.tabs.create({ url: current_req_res_context.edit_url }, function(t) {}); }
     }
       
     if (notificationId === "atc-addtocal-n-addactionresult-bad") {
-      if (buttonIndex == 0) { addStringToCal(current_req_res_context.current_text); }
+      if (buttonIndex == 0) { addStringToCal(current_req_res_context.text_to_add, current_req_res_context.calendar_id, current_req_res_context.caller_cal_nickname); }
+      else if (buttonIndex == 1) { chrome.tabs.create({ url: "https://github.com/nishanths/QuickCal/blob/master/README.md" }, function(t) {}); }
     }
   });
-
+  
+  // Set the options global variable with the changed options
+  chrome.storage.onChanged.addListener(function() {
+    console.log("Changes to storage detected in background.js");
+    fetchOptions();
+  });
 
   /**************************** OPTIONS ******************************/
   
+  // Returns an object of the options
+  function fetchOptions() {
+    chrome.storage.sync.get(null, function(items){
+      // Since we know the properties, set manually instead of iterating because it is more efficient
+      options.mode = items.mode;
+      options.calendar_ids = items.calendar_ids;
+      options.notificationClickEdit = false;
+      options.locale = "en-us";
+      console.log(options);
+      redoContextMenus();
+    });
+  }
   
   /**************************** CONTEXT MENU ******************************/
+  
+  function createSingleMenu_withNickname_andId(nickname, id) {
+    chrome.contextMenus.create({ // Single menu option
+      type    : "normal", // the default textual option
+      id      : "atc-addtocal-cm" + nickname, // for later reference (clearing)
+      title   : "Add to Calendar", // the text that is displayed
+      contexts  : ["selection"], // only display on selected text
+      onclick   : function (obj) { addStringToCal(obj.selectionText, id); }, // attempt cal add
+      documentUrlPatterns: ["<all_urls>"] // show on all addresses
+    });
+  }
+  
+  // Mutiple menus, also passes nickname to the main event adding function
+  function createMultiStyleMenus_withNickName_andId(nickname, id) {
+    chrome.contextMenus.create({
+      type    : "normal", 
+      id      : "atc-addtocal-cm-" + nickname, 
+      title   : "Add to " + nickname,
+      contexts  : ["selection"],
+      onclick   : function (obj) { addStringToCal(obj.selectionText, id, nickname); },
+      documentUrlPatterns: ["<all_urls>"]
+    });
+  }
   
   function removeAllContextMenus() {
     chrome.contextMenus.removeAll();
     console.log("Removed all context menus.");
   }
   
+  // Removes all + Creates depending on options
   function redoContextMenus() {
     removeAllContextMenus();
     createContextMenus();
@@ -74,63 +118,57 @@
   
   // Create context menus; create only one if there is only one calendar specified in options
   function createContextMenus() {
-    var single = { "nickname" : "primary" };
-    var l = 0;
+    var l = 0; // Count
     
-    if (options.mode === "advanced") { // get number of items in advanced, set single on each pass 
-      for (var calendar_nickname in options.calendar_ids) {
-        if (options.calendar_ids.hasOwnProperty(calendar_nickname)) { 
-          l += 1; 
-          single.nickname = options.calendar_ids[calendar_nickname]; } 
+    options.calendar_ids.forEach(function(cal) {
+      if (cal.name && cal.id) {
+        l += 1;
       }
+    });
+        
+    if (options.mode === "simple" || l < 1) { // .Simple (or) chose .Advanced and did not specify anything
+      createSingleMenu_withNickname_andId("primary", "primary");
+      console.log(l + "blueh");
+      
+    }
+   
+    else if (options.mode === "advanced" && l === 1) { // .Advanced and specified one single calendar (cannot POST to 'primary' url) (and) (display one menu option with non-specialized title)       
+      var single = {
+        "nickname" : options.calendar_ids[0].name,
+        "id"       : options.calendar_ids[0].id
+      };
+      createSingleMenu_withNickname_andId(single.nickname, single.id);
     }
     
-    if (options.mode === "simple" || l <= 1) { // single menu option 
-      chrome.contextMenus.create({
-        type    : "normal", // the default textual option
-        id      : "atc-addtocal-cm" + single.nickname, // for later reference (clearing)
-        title   : "Add to Calendar", // the text that is displayed
-        contexts  : ["selection"], // only display on selected text
-        onclick   : function (obj) { addStringToCal(obj.selectionText, single.nickname); }, // attempt cal add
-        documentUrlPatterns: ["<all_urls>"] // show on all addresses
-      });
-    } 
     
-    else { // multiple menus with nicknames 
-      for (var calendar_nickname in options.calendar_ids) {
-        if (options.calendar_ids.hasOwnProperty(calendar_nickname)) {
-          chrome.contextMenus.create({
-            type    : "normal", // the default textual option
-            id      : "atc-addtocal-cm-" + calendar_nickname, 
-            title   : "Add to " + calendar_nickname, // the text that is displayed
-            contexts  : ["selection"], // only display on selected text
-            onclick   : function (obj) { addStringToCal(obj.selectionText, options.calendar_ids[calendar_nickname]); }, // attempt cal add
-            documentUrlPatterns: ["<all_urls>"] // show on all addresses
-          });
+    else { // .Advanced with multiple calendars
+      options.calendar_ids.forEach(function(cal) {
+        if (cal.name && cal.id) { // not empty strings
+          createMultiStyleMenus_withNickName_andId(cal.name, cal.id);
         }
-      }
-    }
+      });
+    }    
   }
     
   /**************************** DATE HELPERS ******************************/
     
   function isToday_NotDateTime(d) {
-      var t = new Date.today().addDays(-1);
-      return d.toDateString() == t.toDateString();
-    }
+    var t = new Date.today().addDays(-1);
+    return d.toDateString() == t.toDateString();
+  }
   
-    function isTomorrow_NotDateTime(d) {
-      return d.toDateString() == (new Date()).toDateString();
-    }
+  function isTomorrow_NotDateTime(d) {
+   return d.toDateString() == (new Date()).toDateString();
+  }
   
-    function isToday_DateTime(d) {
-      return d.toDateString() == (new Date()).toDateString();
-    }
+  function isToday_DateTime(d) {
+    return d.toDateString() == (new Date()).toDateString();
+  }
   
-    function isTomorrow_DateTime(d) {
-      var t = new Date.today().addDays(1);
-      return d.toDateString() == t.toDateString();
-    }
+  function isTomorrow_DateTime(d) {
+    var t = new Date.today().addDays(1);
+    return d.toDateString() == t.toDateString();
+  }
     
   /**************************** DATE FORMATTING ******************************/
     
@@ -155,7 +193,11 @@
       h -= 12;
       suffix = "PM";
     }
-            
+    
+    if (suffix === "midnight" || suffix === "noon") {
+      return h.toString() + space + suffix;
+    }
+      
     return h.toString() + ":" + m_string + space + suffix;
   }
 
@@ -200,7 +242,7 @@
 
 /********************************* RUN ***********************************/
 
-createContextMenus();
+  createContextMenus();
 
   // Construct Google's oauth object
   var google = new OAuth2('google', {
@@ -211,12 +253,15 @@ createContextMenus();
   });
 
   // Add to Google Calendar
-  function addStringToCal(text_to_add, calendar_id) { 
+  function addStringToCal(text_to_add, calendar_id, caller_cal_nickname) { 
     google.authorize(function(){
       var GCAL_POST_URL = 'https://www.googleapis.com/calendar/v3/calendars/' + calendar_id + '/events/quickAdd';
       console.log(GCAL_POST_URL);
       var xhr = new XMLHttpRequest();
-      current_req_res_context.current_text = text_to_add;
+      
+      current_req_res_context.text_to_add = text_to_add;
+      current_req_res_context.calendar_id = calendar_id;
+      current_req_res_context.caller_cal_nickname = caller_cal_nickname;
     
       xhr.onreadystatechange = function(event) {
         if (xhr.readyState == 4) {
@@ -224,7 +269,7 @@ createContextMenus();
             var res = JSON.parse(xhr.responseText); console.log(res);
               current_req_res_context.edit_url = res.htmlLink;
               var notification_title = "An untitled event was added";
-              var notification_message = {    part1: "",  part2: "" };
+              var notification_message = { part1: "",  part2: "" };
               
               // Start and end information from the reponse
               var start = { includesTime: !!res.start.dateTime, date_string: res.start.dateTime || res.start.date };
@@ -233,7 +278,7 @@ createContextMenus();
               var end_date_obj = new Date(end.date_string);
               
               // Event title exists?
-              if (res.summary)  { notification_title = res.summary;}
+              if (res.summary)  { notification_title = res.summary; }
                                                   
               // Time vs all-day
               if (start.includesTime) {
@@ -249,6 +294,32 @@ createContextMenus();
               var date = start_date_obj.getDate();
               if (!start.includesTime) date += 1;
               date = date.toString();
+              
+              // Event successfully added message
+              var contextMessage = "Event added";
+              if (!(typeof caller_cal_nickname === "undefined")) { // defined when there are multiple calendars
+                contextMessage += " to " + caller_cal_nickname + " calendar";
+              }
+              
+              // Compute duration information
+              // if (start.includesTime && end.includesTime) {
+              //   var units = "minutes";
+              //   var diff = (end_date_obj - start_date_obj) / (60*1000);
+              //   var that_diff = diff;
+              //
+              //   if (diff <= 1) units = "minute";
+              //   if (diff <= 60) {
+              //    units = "hours";
+              //    diff = diff / 60;
+              //    diff = Math.round(diff);
+              //    if (diff <= 1) units = "hour";
+              //   }
+              //
+              //   if (!(that_diff > 24*60*2)) {
+              //     if (contextMessage) contextMessage += " (" + diff.toString() + " " + units + " long)";
+              //     else contextMessage = diff.toString() + " " + units + " long"
+              //   }
+              // }
                       
               // Clear and create notification
               chrome.notifications.clear("atc-addtocal-n-addactionresult", function (a) {});
@@ -258,9 +329,9 @@ createContextMenus();
                 iconUrl: paths.remote_cal_date_notification_url + (start_date_obj.getMonth() + 1).toString() + "/" + date + ".png",
                 title: notification_title,
                 message: notification_message.part1,
-                contextMessage: "Event added",
-                  buttons: [{title: "Edit event"}],
-                  isClickable: true
+                contextMessage: contextMessage,
+                buttons: [{title: "Edit event"}],
+                isClickable: true
               }, function(a){});
           } 
                   
@@ -272,7 +343,7 @@ createContextMenus();
               iconUrl: paths.root + paths.dark_icon, 
               title: "Event not added", 
               message:"Something messed up and the event wasn't added.",
-              buttons: [{title: "Try again?"}]
+              buttons: [{title: "Try again?"}, {title: "Get help"}]
             }, function(a){});
           }
         }
